956bd3186bf320dd81e1828851348f855fdbcdeb
Blake Aghili <peopleofksmo@gmail.com>
2020-07-04 11:13:12 -0500

    Static Repo for now.

---
+++ Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static readonly List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+
+        public static IEnumerable<GuestResponse> Responses => responses;
+    }
+}
